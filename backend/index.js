b97a1b6d5c37dd048cf6aa04cb2f51883cf04685
Divyansh Agrawal <divyansh1001agrawal@gmail.com>
2025-10-05 12:45:45 +0530

    commiting files

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [process.env.FRONTEND_URL ,'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
